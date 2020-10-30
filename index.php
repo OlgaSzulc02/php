f5c533b6ba17e51650fe352acc099d584e4e7ecc
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-10-30 11:44:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-echo "hello";
+<?php 
+$servername = "sql7.freemysqlhosting.net"; 
+$username = "sql7373516"; 
+$password = "ShsEsc4Kim"; 
+$dbname = "sql7373516";  
+
+$conn= new mysqli($servername,$username,$password,$dbname);  
+$sql ="select * from pracownicy"; 
+echo("<h3>ZAD.1</h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
 ?>
