f36cd5872a876cf428b058f635a69f52a90ba3fe
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-05 15:10:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $dbname = "sql7373522";
 
 $conn= new mysqli($servername,$username,$password,$dbname);  
 $sql ="select * from pracownicy"; 
-echo("<h3>tabelka</h3>"); 
+echo("<h3>tabelka pracowników</h3>"); 
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
@@ -22,6 +22,19 @@ while($row=mysqli_fetch_assoc($result)){
   echo("<tr>");     
   echo("<tr>");     
   echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+  
+  $sql ="select * from organizacja"; 
+echo("<h3>tabelka organizacja</h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>id_org</th><th>nazwa_dzial</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['id_org']."</td><td>".$row['nazwa_dzial']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
