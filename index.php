9af3945538db9c3908b921f285f78e5b9f223121
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-05 10:19:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -51,6 +51,19 @@ while($row=mysqli_fetch_assoc($result)){
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
+  
+        $sql ="select imie,zarobki from pracownicy where imie not like '%a' having zarobki<40"; 
+echo("<h3>zadanie3</h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>imie</th><th>zarobki</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
 ?>
   </body>
 </html>
