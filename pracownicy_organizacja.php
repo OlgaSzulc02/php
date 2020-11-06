753287bc65302a178f5c04cb29bce8af9cf9961e
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-06 11:39:45 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -17,15 +17,15 @@ $password = "6cxJ7qTmxi";
 $dbname = "sql7374844";  
 
 $conn= new mysqli($servername,$username,$password,$dbname);  
-$sql ="select * from pracownicy"; 
-echo("<h3>tabelka</h3>"); 
+$sql ="select imie,nazwa_dzial from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
+echo("<h3>Pracownicy z nazwą działu</h3>"); 
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+echo ("<tr><th>imie</th><th>nazwa_dzial</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");     
+  echo("<td>".$row['imie']."</td><td>".$row['nazwa_dzial']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
