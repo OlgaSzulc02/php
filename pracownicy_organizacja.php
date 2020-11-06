8f0091af97a07280d79c781eedcc5117ac9ebb24
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-06 11:25:38 +0100

    Update pracownicy_organizacja.php

--- pracownicy_organizacja.php
+++ pracownicy_organizacja.php
@@ -12,9 +12,9 @@
 
 <?php 
 $servername = "sql7.freemysqlhosting.net"; 
-$username = "sql7373522"; 
-$password = "hIFM36Iblk"; 
-$dbname = "sql7373522";  
+$username = "sql7374844"; 
+$password = "6cxJ7qTmxi"; 
+$dbname = "sql7374844";  
 
 $conn= new mysqli($servername,$username,$password,$dbname);  
 $sql ="select * from pracownicy"; 
