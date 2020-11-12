e18290181af65b8f3e1a25f6dc3ca84cae265ab5
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-12 09:19:39 +0100

    Create data_i_czas.php

---
+++ data_i_czas.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Data i Czas</h1>
+  <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a>
+  
+    <div class="nav">
+      <a href="index.php">Strona Główna</a>
+  </div>
+
+<?php 
+$servername = "sql7.freemysqlhosting.net"; 
+$username = "sql7374844"; 
+$password = "6cxJ7qTmxi"; 
+$dbname = "sql7374844";  
+
+$conn= new mysqli($servername,$username,$password,$dbname);  
+$sql ="select year(curdate())-year(data_urodzenia) from as wiek pracownicy"; 
+echo("<h3>Suma zarobków w poszczególnych działach </h3>");
+    echo("<li>".$sql);
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>wiek</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['wiek']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+    
+?>
+  </body>
+</html>
