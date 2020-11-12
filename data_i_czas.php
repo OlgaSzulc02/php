0ba9e7c6b6671aa9c088cbe44c01bc79cc217f4c
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-12 09:22:45 +0100

    Update data_i_czas.php

--- data_i_czas.php
+++ data_i_czas.php
@@ -17,16 +17,16 @@ $password = "6cxJ7qTmxi";
 $dbname = "sql7374844";  
 
 $conn= new mysqli($servername,$username,$password,$dbname);  
-$sql ="select year(curdate())-year(data_urodzenia) as wiek from pracownicy"; 
-echo("<h3>Suma zarobków w poszczególnych działach </h3>");
+$sql ="select imie,year(curdate())-year(data_urodzenia) as wiek from pracownicy"; 
+echo("<h3>Wiek poszczególnych pracowników </h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>wiek</th></tr>"); 
+echo ("<tr><th>imie</th><th>wiek</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['wiek']."</td>");     
+  echo("<td>".$row['imie']."</td><td>".$row['wiek']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
