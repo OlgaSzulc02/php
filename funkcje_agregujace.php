bda26cea9e8b3888606cf135cf5878314f6e260b
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-12 08:27:04 +0100

    Create funkcje_agregujace.php

---
+++ funkcje_agregujace.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Pracownicy i Organizacja</h1>
+  <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-OlgaSzulc02">github</a>
+  
+    <div class="nav">
+      <a href="index.php">Strona Główna</a>
+  </div>
+
+<?php 
+$servername = "sql7.freemysqlhosting.net"; 
+$username = "sql7374844"; 
+$password = "6cxJ7qTmxi"; 
+$dbname = "sql7374844";  
+
+$conn= new mysqli($servername,$username,$password,$dbname);  
+$sql ="select nazwa_dzial, sum(zarobki) from pracownicy,organizacja where id_org=dzial group by nazwa_dzial"; 
+echo("<h3>Suma zarobków w poszczególnych działach </h3>"); 
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>nazwa_dzial</th><th>sum(zarobki)</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['nazwa_dzial']."</td><td>".$row['sum(zarobki)']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+?>
+  </body>
+  </html>
