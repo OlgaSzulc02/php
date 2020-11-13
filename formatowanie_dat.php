2dac5f54c9ce32fcc86857326eaaa721da025424
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-13 15:11:08 +0100

    Update formatowanie_dat.php

--- formatowanie_dat.php
+++ formatowanie_dat.php
@@ -89,16 +89,16 @@ while($row=mysqli_fetch_assoc($result)){
   echo("</tr>"); } 
 echo('</table>'); 
     
-     $sql = "select datediff(curdate(),'2002-08-01') as dni,datediff(curdate(),'2002-08-01')*24 as godz,datediff(curdate(),'2002-08-01')*24*60 as min";
+     $sql = "select datediff(curdate(),'2002-08-01')*24 as godz,datediff(curdate(),'2002-08-01')*24*60 as min";
 echo("<h3>Ile godzin, minut już żyjesz</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>dni</th><th>godziny</th><th>minuty</th></tr>"); 
+echo ("<tr><th>godziny</th><th>minuty</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['dni']."</td><td>".$row['godz']."</td><td>".$row['min']."</td>");     
+  echo("<td>".$row['godz']."</td><td>".$row['min']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
@@ -113,6 +113,29 @@ while($row=mysqli_fetch_assoc($result)){
   echo("<tr>");     
   echo("<tr>");     
   echo("<td>".$row['ur']."</td>");     
+  echo("<tr>"); 
+  echo("</tr>"); } 
+echo('</table>'); 
+
+$sql = "select imie, date_format(data_urodzenia,'%W') as dzien,nazwa_dzial from pracownicy,organizacja where id_org=dzial order by 
+CASE
+  WHEN dzien = 'Poniedziałek' THEN 1
+  WHEN dzien = 'Wtorek' THEN 2
+  WHEN dzien = 'Środa' THEN 3
+  WHEN dzien= 'Czwartek' THEN 4
+  WHEN dzien = 'Piątek' THEN 5
+  WHEN dzien = 'Sobota' THEN 6
+  WHEN dzien = 'Niedziela' THEN 7
+END ASC";
+    echo("<h3>Pracownicy z nazwami dni tygodnia, w których się urodzili z sortowaniem od Poniedziałku do Niedzieli</h3>");
+    echo("<li>".$sql);
+$result = mysqli_query($conn, $sql);  
+echo('<table border="1" class="tabela"'); 
+echo ("<tr><th>imie</th><th>dzien</th><th>nazwa_dzial</th></tr>"); 
+while($row=mysqli_fetch_assoc($result)){ 
+  echo("<tr>");     
+  echo("<tr>");     
+  echo("<td>".$row['imie']."</td><td>".$row['dzien']."</td><td>".$row['nazwa_dzial']."</td>");       
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
