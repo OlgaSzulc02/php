aa04bcef8229fe960d5c4c7e7111b368d7990428
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-13 15:58:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ echo('</table>');
     echo("<h2>Zadania</h2>"); 
     
     $sql ="select imie from pracownicy where (dzial=2)"; 
-echo("<h3>zadanie1</h3>"); 
+echo("<h3>Pracownicy tylko z działu 2</h3>"); 
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -65,7 +65,7 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
     
         $sql ="select imie from pracownicy where (dzial=2 or dzial=3)"; 
-echo("<h3>zadanie3</h3>"); 
+echo("<h3>Pracownicy tylko z działu 2 i z działu 3</h3>"); 
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
@@ -79,7 +79,7 @@ while($row=mysqli_fetch_assoc($result)){
 echo('</table>'); 
     
         $sql ="select imie from pracownicy where (zarobki<30)"; 
-echo("<h3>zadanie2</h3>"); 
+echo("<h3>Pracownicy tylko z zarobkami mniejszymi niż 30</h3>"); 
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
