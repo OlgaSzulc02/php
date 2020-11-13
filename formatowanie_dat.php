10c821ca9624b0b9560bf4942cf85c659091db7a
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-13 13:49:43 +0100

    Update formatowanie_dat.php

--- formatowanie_dat.php
+++ formatowanie_dat.php
@@ -17,17 +17,17 @@ $password = "6cxJ7qTmxi";
 $dbname = "sql7374844";  
 
 $conn= new mysqli($servername,$username,$password,$dbname);  
-
-$sql ="select date_format(data_urodzenia,'%w-%m-%y') as data from pracownicy"; 
+    
+ $sql = "select imie, date_format(data_urodzenia, '%W-%M-%Y') as data from pracownicy";
 echo("<h3>Wyświetl nazwy dni w dacie urodzenia</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>data</th></tr>"); 
+echo ("<tr><th>imie</th><th>data</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['data']."</td>");     
+  echo("<td>".$row['imie']."</td><td>".$row['data']."</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
