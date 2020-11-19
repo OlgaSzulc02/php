2638eae1b9a6754183220de3e9c721b914b6d6da
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-19 09:14:07 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -5,6 +5,9 @@ echo("To strona.php");
 echo("<ul>");
 	
 echo("<li>".$_POST["imie"]);
-		
+echo("<li>".$_POST["nazwisko"]);
+echo("<li>".$_POST["miasto"]);
+echo("<li>".$_POST["telefon"]);
+echo("<li>".$_POST["kod pocztowy"]);
 echo("<ul>");
 ?>
