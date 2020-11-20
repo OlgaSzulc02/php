ff0e73645fbae482f147a1dc8b821565d6b90512
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-20 10:26:03 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,29 @@
+<?php
+echo("Delete<br>");
+echo $_POST['id'];
+
+
+$servername = "sql7.freemysqlhosting.net"; 
+$username = "sql7374844"; 
+$password = "6cxJ7qTmxi"; 
+$dbname = "sql7374844";    
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM Pracownik WHERE id= $_POST['id'];";
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
