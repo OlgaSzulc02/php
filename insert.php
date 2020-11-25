0f549ef4061ae91f6c8b04a9fb367494ee1721b3
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-25 15:37:12 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,28 @@
+<?php
+echo("Insert");
+echo $_POST['name'];
+
+$servername = "sql7.freemysqlhosting.net"; 
+$username = "sql7374844"; 
+$password = "6cxJ7qTmxi"; 
+$dbname = "sql7374844";    
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (id,imie, dzial, zarobki,data_urodzenia) 
+       VALUES (null,'".$_POST['name']."', 1, 76,'1991-11-21')";
+
+echo "<li>".$sql;
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
