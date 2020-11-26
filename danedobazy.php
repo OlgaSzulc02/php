411230a37347f29517e9bab61879e9e3b9f0eb10
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-26 09:38:20 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="style.css">
-  <meta charset="utf-8">
+
   <meta name="viewport" content="width=device-width">
   <title>Dodawanie i Usuwanie</title>
 </head>
