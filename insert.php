c304e1d38633cd5de05fc2a7294e161a21900461
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-11-27 15:53:42 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -18,7 +18,7 @@ echo "<li>".$sql;
 
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  header('Location: https://olga-szulc.herokuapp.com/danedobazy.php');
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
