f33ecabd69edf225654edcd0698e31e085860bf0
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-02 18:07:00 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,9 +4,7 @@ echo $_POST['id_pracownicy'];
 
 
 require_once("connect.php");
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+
 
 
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
