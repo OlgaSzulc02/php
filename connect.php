3f7abcee312245d54d6542c89844c09da904058c
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-02 19:44:46 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "127.0.0.1 via TCP/IP";
-$username = "root@localhost";
-$password = "x+f!!3dB!UuH$?6";
-$dbname = "test";
+$servername = "mysql-olgaszulc.alwaysdata.net";
+$username = "olgaszulc";
+$password = "GSeDfKG.5!QZfk3";
+$dbname = "olgaszulc_pbd";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
