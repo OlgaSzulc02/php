5bfcaf6dcf59fe6239cfe51360239c5ba4b3f7e5
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-02 18:06:43 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,15 +1,6 @@
 <?php
 echo("Insert");
-
-$servername = "sql7.freemysqlhosting.net"; 
-$username = "sql7378763"; 
-$password = "tKgKu1mp5I"; 
-$dbname = "sql7378763";    
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once("connect.php");
 
 $sql = "INSERT INTO pracownicy (id_pracownicy,imie, dzial, zarobki,data_urodzenia) 
        VALUES (null,".'"'.$_POST['imie'].'"'.','.$_POST['dzial'].','.$_POST['zarobki'].','.'"'.$_POST['data_urodzenia'].'"'.')';
