2dd58fc5c04846fb8387ee5ea5e1451dd99b7142
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-03 09:11:39 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -39,11 +39,11 @@ echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>""<td>
-  <form action="delete.php" method="POST">
-  <input type="number" name="id_pracownicy"</br>
-  <input type="submit" value="usuń pracownika">
-  </form>)</td>";     
+  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>"."<td>
+<form action="delete.php" method="POST">
+<input type="number" value="'.$wiersz['id_pracownicy'].'" hidden>
+<input type="submit" value="usuń pracownika">
+	     </form></td>";     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
