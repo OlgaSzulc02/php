9d05a488853f0d790c88b8ca3c30d1a223b9b539
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-03 09:17:57 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -35,15 +35,17 @@ echo("<h3>tabelka</h3>");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th><th>Usuń Pracownika</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>"."<td>
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td><td>'.$row['dzial'].'</td><td>'.$row['nazwa_dzial'].'</td>'.
+'<td>
 <form action="delete.php" method="POST">
-<input type="number" value="'.$wiersz['id_pracownicy'].'" hidden>
-<input type="submit" value="usuń pracownika">
-	     </form></td>";     
+<input name="id" value="'.$row['id_pracownicy'].'" hidden>
+<input type="submit" value="Usuń pracownika">
+</form>
+</td>');
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
