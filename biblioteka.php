32e192b1f50c28c130fb8a371d00ef9176e2b0c2
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2020-12-17 09:35:35 +0100

    Update biblioteka.php

--- biblioteka.php
+++ biblioteka.php
@@ -11,15 +11,15 @@
 <?php 
 require_once("connect.php");
   echo("<h3>Biblioteka</h3>"); 
-$sql = "select biblAutor_biblTytul as id_at, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where autor=biblAutor_id and tytul=biblTytul_id";
+$sql = "select biblAutor_id, biblTytul_id, autor, tytul, biblWypoz from biblAutor_biblTytul,biblAutor,biblTytul where autor=biblAutor_id and tytul=biblTytul_id";
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);
 echo('<table border="1" class="tabela"'); 
-    echo ("<tr><th>id</th><th>autor</th><th>tytul</th><th>wypozyczenia</th></tr>");
+    echo ("<tr><th>id autora</th><th>id tytulu</th><th>autor</th><th>tytul</th><th>wypozyczenia</th></tr>");
         while ($row = mysqli_fetch_assoc($result)) {
   echo("<tr>");     
   echo("<tr>");   
- echo ('<td>'.$row["id_at"].'</td><td>'.$row["autor"].'</td><td>'.$row["tytul"].'</td><td>'.$row["biblWypoz"].'</td>');
+ echo ('<td>'.$row["biblAutor_id"].'</td><td>'.$row["biblTytul_id"].'</td><td>'.$row["autor"].'</td><td>'.$row["tytul"].'</td><td>'.$row["biblWypoz"].'</td>');
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
