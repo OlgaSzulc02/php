0a479cded0f49479e7ba5aed7cd82589a4f0f340
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-01-28 13:54:44 +0100

    Update biblioteka.php

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -9,6 +9,22 @@
       <a href="index.php">Strona Główna</a>
     </div>
 <?php 
+	require_once("connect.php");
+    echo ('<h2>Biblioteka<h2>');
+	$sql ="select autor, tytul from biblAutor,biblTytul";
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+echo('<select name="ksiazka">');
+	while($row = mysqli_fetch_assoc($result)) {
+            echo '<option value="'.$row['id'].'">';
+	    echo($row['autor'].', '.$row['tytul']);
+ 	    echo "</option>";
+	};
+echo('</select>');
+echo ('<br>');
 require_once("connect.php");
   echo("<h3>Tytuły</h3>"); 
 $sql = "select * from biblTytul";
