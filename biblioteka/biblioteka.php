5b538ecd246393f4e6f17cb3659098be3e2c05fb
OlgaSzulc <63042942+OlgaSzulc02@users.noreply.github.com>
2021-01-28 13:57:35 +0100

    Update biblioteka.php

--- biblioteka/biblioteka.php
+++ biblioteka/biblioteka.php
@@ -11,7 +11,7 @@
 <?php 
 	require_once("connect.php");
     echo ('<h2>Biblioteka<h2>');
-	$sql ="select autor, tytul from biblAutor,biblTytul group by tytul";
+	$sql ="select autor, tytul from biblAutor,biblTytul group by (tytul or autor)";
 $result = mysqli_query($conn, $sql);
 if ( $result) {
     } else {
