0b997fa6389ea68ab86e16c92adbe2a4c3660397
OlgaSzulc02 <olga.szulc24@gmail.com>
2021-09-09 20:30:01 +0200

    ssss

--- tak/plik2.php
+++ tak/plik2.php
@@ -7,6 +7,24 @@
     <body>
         <div class="pudelko">
             <div class="p1">
+            <h3>dodawanie pracownika</h3>
+<form action="insert.php" method="POST">
+
+				
+			<label>Imie:</label><input type="text" name="imie"><br>
+	
+			<label>Dzial:</label><input type="text" name="dzial"></br>
+				
+			<label>Zarobki:</label><input type="text" name="zarobki"></br>
+	
+			<label>Data urodzenia:</label><input type="date" name="data_urodzenia"></br>
+				
+			<input type="submit" value="dodaj pracownika">
+</form>
+<form action="delete.php" method="POST">
+	<label>Podaj id:</label><input type="number" name="id_pracownicy"</br>
+   <input type="submit" value="usuń pracownika">
+</form>
             <?php
 
 echo("<h2>Zadania</h2>"); 
