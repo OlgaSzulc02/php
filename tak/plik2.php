f0c16a9025e5b6f54a0a8474494d184e2ebbaa8f
OlgaSzulc02 <olga.szulc24@gmail.com>
2021-09-09 20:47:44 +0200

    s

--- tak/plik2.php
+++ tak/plik2.php
@@ -27,19 +27,16 @@
 </form>
             <?php
 
-echo("<h2>Zadania</h2>"); 
-require_once("connect.php");
-    $sql ="select * from pracownicy"; 
-echo("<h3>Pracownicy"); 
+echo("<h2>Podstawowe</h2>"); 
+$sql ="select * from pracownicy"; 
+echo("<h3>tabelka</h3>"); 
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
 echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
-while($row=mysqli_fetch_assoc($result)){ 
+while($row=mysqli_fetch_assoc($result)){  
   echo("<tr>");     
-  echo("<tr>");     
-  echo("<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>");     
-  echo("<tr>"); 
+echo('<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>');
   echo("</tr>"); } 
 echo('</table>'); 
 ?>
