4492f918e23ba71a29b354e553e31564863a67d9
OlgaSzulc02 <olga.szulc24@gmail.com>
2021-09-09 20:40:29 +0200

    ssss

--- tak/plik2.php
+++ tak/plik2.php
@@ -34,11 +34,11 @@ echo("<h3>Pracownicy");
     echo("<li>".$sql);
 $result = mysqli_query($conn, $sql);  
 echo('<table border="1" class="tabela"'); 
-echo ("<tr><th>imie</th></tr>"); 
+echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>"); 
 while($row=mysqli_fetch_assoc($result)){ 
   echo("<tr>");     
   echo("<tr>");     
-  echo("<td>".$row['imie']."</td>");     
+  echo("<td>'.$row['id_pracownicy'].'</td><td>'.$row['imie'].'</td><td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>");     
   echo("<tr>"); 
   echo("</tr>"); } 
 echo('</table>'); 
